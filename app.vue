9d6fc9992b71543504b2e37e245cf0cf3c702e71
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-11-28 20:17:01 +0100

    update app layout

--- app.vue
+++ app.vue
@@ -14,16 +14,35 @@ const {
 <template>
   <div class="container flex flex-col justify-center items-center mx-auto gap-10 p-10">
     <header>
-      <h1 class="text-5xl font-bold text-center">Visa Calculator</h1>
+      <h1 class="text-5xl font-bold text-center my-10">Visa Calculator</h1>
     </header>
 
-    <!-- enter date -->
-    <form-control label="Enter Date" type="date" v-model="enterDate" />
+    <div class="flex flex-col md:flex-row w-full items-center justify-center gap-10">
+      <!-- enter date -->
+      <form-control label="Enter Date" type="date" v-model="enterDate" />
 
-    <!-- exit date -->
-    <form-control label="Exit Date" type="date" v-model="exitDate" :disabled="!canExit" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        class="max-md:hidden w-8 h-8 opacity-50 mt-8"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"
+        />
+      </svg>
 
-    <stat-group class="max-md:stats-vertical md:my-10">
+      <!-- exit date -->
+      <form-control
+        label="Exit Date"
+        type="date"
+        v-model="exitDate"
+        :disabled="!canExit"
+      />
+    </div>
+
+    <stat-group class="max-md:stats-vertical md:my-10 max-sm:w-full">
       <stat title="Used Days">
         <countdown :value="usedDays" />
         <span class="opacity-50"> / {{ MAX_DAYS }} </span>
