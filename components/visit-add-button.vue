cf4859c18ffeb14cf6cbdad9d5df93b6d7bc064d
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-02 16:37:22 +0100

    add visit action buttons

---
+++ components/visit-add-button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button" class="btn btn-primary btn-outline btn-lg btn-wide rounded-full">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      class="bi bi-plus-lg w-8 h-8"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
+      />
+    </svg>
+  </button>
+</template>
